ea6af8755d450ac534dc5c1882ba8a131331d503
Chloe <cugttweb@pm.me>
2020-05-23 18:23:21 +0100

    making a start on desktop styles

--- insure-landing-page-master/main.js
+++ insure-landing-page-master/main.js
@@ -1,8 +1,6 @@
 const hamburgerIcon = document.getElementById('hamburger');
 const mainNav = document.querySelector('.main-nav');
 
-console.log(screen.width)
-
 // toggle class to show/hide mobile navigation
 hamburgerIcon.addEventListener('click', () => {
     mainNav.classList.toggle('show-hide');
