f235ba15fa548976790c0f1cc02c8f42c724882d
Chloe <cugttweb@pm.me>
2020-11-25 17:57:51 +0000

    starting desktop styles on insure LP

--- insure-landing-page-master/main.js
+++
@@ -1,7 +0,0 @@
-const hamburgerIcon = document.getElementById('hamburger');
-const mainNav = document.querySelector('.main-nav');
-
-// toggle class to show/hide mobile navigation
-hamburgerIcon.addEventListener('click', () => {
-    mainNav.classList.toggle('show-hide');
-})
